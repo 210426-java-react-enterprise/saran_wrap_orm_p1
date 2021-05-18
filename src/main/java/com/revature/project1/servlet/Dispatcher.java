b1fd19b16866d9a25bacecd8f54999a9ba93d66c
deterge <45431127+Deterge@users.noreply.github.com>
2021-05-18 17:39:53 -0400

    added files related to web.xml, updated web.xml with patterns, updated pom.xml with Jackson dependency, skeleton for Dispatcher

--- src/main/java/com/revature/project1/servlet/Dispatcher.java
+++ src/main/java/com/revature/project1/servlet/Dispatcher.java
@@ -1,5 +1,29 @@
 package com.revature.project1.servlet;
 
+import com.revature.project1.models.AppUser;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 public class Dispatcher {
     Dispatcher () {}
+
+    public void dispatch (HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        switch (req.getRequestURI()) {
+            // /registration.data, /authentication.data, /transaction.data, /delete.data
+            // based on /pattern in web.xml file
+            case "p0/registration.data":
+                break;
+            case "p0/authentication.data":
+                break;
+            case "p0/transaction.data":
+                break;
+            case "p0/delete.data":
+                break;
+            default:
+                resp.setStatus(400);
+                resp.getWriter().println("Invalid Request");
+        }
+    }
 }
