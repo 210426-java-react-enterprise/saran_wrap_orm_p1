6d1bfdf156e966719e6bd9503fd61e0fda92acde
Giancarlo Tomasello <gtomasel@ucsc.edu>
2021-05-21 15:29:31 -0700

    Changed the way I got the column names and column values. Added them to the sql insert string

--- src/main/java/com/revature/project1/dbentry/SqlInsert.java
+++ src/main/java/com/revature/project1/dbentry/SqlInsert.java
@@ -1,11 +1,13 @@
 package com.revature.project1.dbentry;
 
+import com.revature.project1.annotations.Column;
 import com.revature.project1.annotations.Entity;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -27,57 +29,35 @@ public class SqlInsert {
 
         }
 
-
         //Get all the fields in the pojo
         Field[] declaredClassFields = clazz.getDeclaredFields();
         System.out.println("Field 1 name " + declaredClassFields[1].getName());
-        //Use the field annotations and get all the column names
-        ArrayList<Field> columnNames = new ArrayList<>();
+
+        //Use the field annotations and get all the column names and values and put them in their own ArrayLists
+        ArrayList<String> columnNames = new ArrayList<>();
+        ArrayList<String> columnValues = new ArrayList<>();
         for(Field fields: declaredClassFields){
             Annotation[] fieldAnnotations = fields.getAnnotations();
 
             for (Annotation anno: fieldAnnotations){
-
                 String annoName = anno.annotationType().getSimpleName();
-
                 if(annoName.equals("Column")){
-                    columnNames.add(fields);
+                    columnNames.add(fields.getName());
+                    fields.setAccessible(true);
+                    columnValues.add( fields.get(obj).toString());
+                    fields.setAccessible(false);
                 }
             }
-        }
-
-        //Mapped the column names and their values
-        Map<String, Object> columnValues = new HashMap<String, Object>();
-
-        for(Field names: columnNames){
-            names.setAccessible(true);
-            columnValues.put(names.getName(), names.get(obj));
-           // System.out.println("Field Value: " + names.get(obj));
-            //System.out.println("Map value: " + columnValues.get(names.getName()));
-            //System.out.println("Field Name: " + names);
-            //System.out.println("Field Value: " + columnValues.get(names.getName()));
-            names.setAccessible(false);
         }
 
         //Create the start of sql string and adding table name
         String sqlInsert = "Insert into ";
         sqlInsert += tableName + " (";
 
-        //Adds all the column ames but adds a , at the end will cause errors in the sql call
-//        for(Field names: columnNames){
-//            sqlInsert += names.getName()+",";
-//        }
+        //Adds the column names and column values to the sql string.
+        sqlInsert += String.join(",", columnNames) + ")";
+        sqlInsert += " values (" + String.join(",", columnValues) + ")";
 
-        //Adds the keys to the list but sorts them. Might cause some issues with aligning the values
-        String temp = String.join(",", columnValues.keySet());
-        System.out.println("Stream string: " + temp);
-        sqlInsert += temp + ") values (";
-
-
-
-        //sqlInset + tableName
-        //SqlInsert + ( + all column names + )
-        //SqlInsert values ( + all vales + )
 
         System.out.println("Sql String: " + sqlInsert);
 
