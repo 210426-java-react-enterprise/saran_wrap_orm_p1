d38c3ee3d342ff77648f0739b0544d0ebbb39803
Giancarlo Tomasello <gtomasel@ucsc.edu>
2021-05-26 18:25:51 -0700

    Created a saranServices method that will be used to call all the other CRUD method. Tweaked servlet to test connections

---
+++ src/main/java/com/revature/project1/services/SaranServices.java
@@ -0,0 +1,27 @@
+package com.revature.project1.services;
+
+import com.revature.project1.dbentry.SqlInsert;
+import com.revature.project1.util.ConnectionFactory;
+
+import java.sql.Connection;
+
+public class SaranServices {
+
+    Connection conn;
+
+    public SaranServices(){
+        conn = ConnectionFactory.getInstance().getConnection();
+    }
+
+    //Inserting one obj into a database
+    public void insertInDB(Object obj){
+        SqlInsert insertTest = new SqlInsert();
+
+
+        try {
+            insertTest.insertNewObject(obj, conn);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/com/revature/project1/servlet/Dispatcher.java
+++ src/main/java/com/revature/project1/servlet/Dispatcher.java
@@ -1,6 +1,7 @@
 package com.revature.project1.servlet;
 
 import com.revature.project1.models.AppUser;
+import com.revature.project1.services.SaranServices;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,6 +14,16 @@ public class Dispatcher {
         switch (req.getRequestURI()) {
             // /registration.data, /authentication.data, /transaction.data, /delete.data
             // based on /pattern in web.xml file
+            case "saranwarap/ConnectionTest":
+                System.out.println("Running connection test");
+                Object userObj = new AppUser("gtomasel", "Passw0rd", "Email@mail.com", "Giancarlo", "Lastname", 23);
+
+                System.out.println("Attempting to create a connection");
+                SaranServices saran = new SaranServices();
+
+                System.out.println("Attempting To add to DB");
+                saran.insertInDB(userObj);
+                break;
             case "saranwrap/registration.data":
                 break;
             case "saranwrap/authentication.data":
