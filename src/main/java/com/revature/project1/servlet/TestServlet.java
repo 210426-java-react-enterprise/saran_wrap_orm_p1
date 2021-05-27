104d3578390a5f0a808ee7b311e7942f33e7ccae
Giancarlo Tomasello <gtomasel@ucsc.edu>
2021-05-26 21:00:25 -0700

    Fixed the weird issue with the pom.xml. Now I can test the project. Remember to Undeplot on application manager or else you will have issues

--- src/main/java/com/revature/project1/servlet/TestServlet.java
+++ src/main/java/com/revature/project1/servlet/TestServlet.java
@@ -5,9 +5,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class testServlet extends HttpServlet {
+public class TestServlet extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setStatus(400);
         resp.getWriter().write("<h1>/test works!</h1>");
     }
+
 }
